8539e78d870c68cbfa535e332fa6ee4cfc7fd815
GaoWenSheng <gao0831mail@163.com>
2026-01-06 13:53:17 +0800

    notice 通知模块 - Completed

---
+++ os-common/common-notice/src/main/java/com/goalias/common/notification/core/AliyunSmsTemplate.java
@@ -0,0 +1,107 @@
+package com.goalias.common.notification.core;
+
+import cn.hutool.json.JSONUtil;
+import com.aliyun.dysmsapi20170525.Client;
+import com.aliyun.dysmsapi20170525.models.SendSmsRequest;
+import com.aliyun.dysmsapi20170525.models.SendSmsResponse;
+import com.aliyun.teaopenapi.models.Config;
+import com.goalias.common.core.exception.UtilException;
+import com.goalias.common.core.utils.StringUtils;
+import com.goalias.common.notification.config.properties.SmsProperties;
+import com.goalias.common.notification.entity.SmsResult;
+import lombok.SneakyThrows;
+
+
+import java.security.SecureRandom;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Aliyun 短信模板
+ *
+ * @author Lion Li
+ * @version 4.2.0
+ */
+public class AliyunSmsTemplate implements SmsTemplate {
+
+    private final SmsProperties properties;
+
+    private final Client client;
+
+    @SneakyThrows(Exception.class)
+    public AliyunSmsTemplate(SmsProperties smsProperties) {
+        this.properties = smsProperties;
+        Config config = new Config()
+            // 您的AccessKey ID
+            .setAccessKeyId(smsProperties.getAccessKeyId())
+            // 您的AccessKey Secret
+            .setAccessKeySecret(smsProperties.getAccessKeySecret())
+            // 访问的域名
+            .setEndpoint(smsProperties.getEndpoint());
+        this.client = new Client(config);
+    }
+
+    private static final String NUMBERS = "0123456789";
+    private static final String ALPHANUMERIC = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
+
+    @Override
+    public SmsResult send(String phones, String templateId, Map<String, String> param) {
+        if (StringUtils.isBlank(phones)) {
+            throw new UtilException("手机号不能为空");
+        }
+        for (String phone : phones.split(",")) {
+            if (!isMobileNum(phone)) {
+                throw new UtilException("手机号格式不正确");
+            }
+        }
+        if (StringUtils.isBlank(templateId)) {
+            throw new UtilException("模板ID不能为空");
+        }
+        SendSmsRequest req = new SendSmsRequest()
+            .setPhoneNumbers(phones)
+            .setSignName(properties.getSignName())
+            .setTemplateCode(templateId)
+            .setTemplateParam(JSONUtil.toJsonStr(param));
+        try {
+            SendSmsResponse resp = client.sendSms(req);
+            return SmsResult.builder()
+                .isSuccess("OK".equals(resp.getBody().getCode()))
+                .message(resp.getBody().getMessage())
+                .response(JSONUtil.toJsonStr(resp))
+                .build();
+        } catch (Exception e) {
+            throw new UtilException(e.getMessage());
+        }
+    }
+
+    /**
+     * 检测手机号有效性*
+     * @param mobile 手机号码
+     * @return 是否有效
+     */
+    public static boolean isMobileNum(String mobile){
+        Pattern p = Pattern.compile("^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$");
+        Matcher m = p.matcher(mobile);
+        return m.matches();
+    }
+
+    public static String randomSMSCode(int length, boolean numberFlag) {
+        if (length <= 0) {
+            throw new IllegalArgumentException("Length must be positive");
+        }
+
+        String chars = numberFlag ? NUMBERS : ALPHANUMERIC;
+        char[] buffer = new char[length];
+
+        // 直接填充 char[]，避免 StringBuilder/String 拼接开销
+        for (int i = 0; i < length; i++) {
+            buffer[i] = chars.charAt(SECURE_RANDOM.nextInt(chars.length()));
+        }
+
+        return new String(buffer);
+    }
+
+}
---
+++ os-common/common-notice/src/main/java/com/goalias/common/notification/core/MailTemplate.java
@@ -0,0 +1,117 @@
+package com.goalias.common.notification.core;
+
+import com.goalias.common.core.exception.UtilException;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.util.Objects;
+
+/**
+ * Mail 邮件模板
+ *
+ * @author Lion Li
+ * @version 4.2.0
+ */
+@Component
+@RequiredArgsConstructor
+public class MailTemplate {
+
+    private final JavaMailSender mailSender;
+
+    private final MailProperties properties;
+
+    /**
+     * 发送简单文本邮件
+     *
+     * @param to      收件人
+     * @param subject 邮件主题
+     * @param text    邮件内容
+     */
+    public void sendSimpleMail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(properties.getUsername());
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+
+    /**
+     * 发送带附件的邮件
+     *
+     * @param to       收件人
+     * @param subject  邮件主题
+     * @param text     邮件内容
+     * @param filePath 附件路径
+     * @throws MessagingException
+     */
+    public void sendMailWithAttachment(String to, String subject, String text, String filePath){//文件存储方式？
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+            helper.setFrom(properties.getUsername());
+            helper.setValidateAddresses(true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text);
+            FileSystemResource file = new FileSystemResource(new File(filePath));
+            helper.addAttachment(Objects.requireNonNull(file.getFilename()), file);
+        } catch (Exception e) {
+            throw new UtilException("附件邮件发送失败");
+        }
+
+        mailSender.send(mimeMessage);
+    }
+
+    /**
+     * 发送HTML格式邮件
+     *
+     * @param to      收件人
+     * @param subject 邮件主题
+     * @param html    HTML内容
+     */
+    public void sendHtmlMail(String to, String subject, String html) {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+            helper.setFrom(properties.getUsername());
+            helper.setValidateAddresses(true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(html, true);
+        } catch (MessagingException e) {
+            throw new UtilException("html邮件发送失败");
+        }
+        mailSender.send(mimeMessage);
+    }
+
+    public String findPasswordTemplate(String accountName, String code, String mainUrl) {
+        return "<body>" +
+                "<div style='margin:0;'><span style='font-size: 20px;'><b>尊敬的用户：</b>" +
+                "</span></div><div style='margin:0;'>&nbsp; &nbsp; &nbsp;您好！（登录账号：<b>" +
+                accountName +
+                "</b>）</div><div style='margin:0;'>您正在通过邮件重置密码，请点击" +
+                "<a href='https://" + mainUrl + "/index?to=4&code=" + code
+                + "' style='text-decoration:none;' taret='_blank'><b>这里</b></a>" +
+                "跳转，" +
+                "</div><div style='margin:0;'>或复制链接：<b>https://"
+                + mainUrl + "/index?to=4&code=" + code
+                + "</b></div><div style='margin:0;'><br></div><div style='margin:0;'>" +
+                "如果您现在想起了您的密码：</div><div style='margin:0;'>" +
+                "您可以忽略上述信息，可继续使用原来的密码登录，无需执行任何操作。" +
+                "</div><div><span style='color: rgb(221, 64, 50);'><b>如果此邮件非您本人操作，您无需理会此邮件。" +
+                "</b></span></div><div style='margin:0;'>这是一封系统自动发出的邮件，请不要直接回复。" +
+                "</div><div style='margin:0;'><br></div><div style='margin:0;'><br></div></body>";
+    }
+
+
+}
